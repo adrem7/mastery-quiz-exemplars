3f05279d3e0ab82123c7c616734c75e38b34bffd
Sam Jones <sam.jones494@googlemail.com>
2019-05-10 11:36:20 +0100

    chapter 9

--- chapter9/quiz1/questions/question_1.rb
+++ chapter9/quiz1/questions/question_1.rb
@@ -58,3 +58,36 @@ def random_card
 
   cards[rand(13)]
 end
+
+def move
+  choice = ''
+  while choice != 'hit' && choice != 'stick'
+    choice = gets.chomp
+  end
+  return choice
+end
+
+def score(hand)
+  scores = {
+    'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
+    'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9,
+    'ten' => 10, 'jack' => 10, 'queen' => 10, 'king' => 10, 'ace' => 11
+  }
+
+  hand.map { |card| scores[card] }.sum
+end
+
+def run_game
+  hand = []
+
+  while true do
+    puts 'hit or stick?'
+    player_choice = move
+    break if player_choice == 'stick'
+    hand.push(random_card)
+    puts "Score so far: #{score(hand)}"
+  end
+
+  puts score(hand) > 21 ?
+    "You busted with: #{score(hand)}" : "You scored: #{score(hand)}"
+end
